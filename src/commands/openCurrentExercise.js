a7a55c76a177dd428cb0c93892b23224e3038f96
Amilcar Erazo <ajose.erazo@gmail.com>
2021-07-26 20:55:51 -0500

    Reseting files

--- src/commands/openCurrentExercise.js
+++ src/commands/openCurrentExercise.js
@@ -1,25 +1,40 @@
-const vscode = require('vscode');
-const lp = require('../learnpack');
-const logger = require('../utils/console')
-
+const vscode = require("vscode");
+const lp = require("../learnpack");
+const logger = require("../utils/console");
+const { createResetFile } = require("../utils");
 
 module.exports = async () => {
-    logger.debug("open current")
-    
-    let current = lp.currentExercise()
-    
-    logger.debug(`Opening ${current.slug}`, current)
-    return openFiles(current)
-    
-}
+  logger.debug("open current");
+
+  let current = lp.currentExercise();
+
+  logger.debug(`Opening ${current.slug}`, current);
+  return openFiles(current);
+};
 
 const openFiles = async (ex) => {
-    const files = ex.visibleFiles;
-    logger.debug(`Files to open`, files)
-    let editor = null
-    for(let i = 0; i < files.length; i++){
-        const file = vscode.Uri.file(`${extension.workspaceRoot}/${files[i].path}`);
-        editor = await vscode.window.showTextDocument(file, vscode.ViewColumn.One, false)
+  const files = ex.visibleFiles;
+  
+  logger.debug(`Files to open`, files);
+  let editor = null;
+  for (let i = 0; i < files.length; i++) {
+    try {
+      const file = vscode.Uri.file(
+        `${extension.workspaceRoot}/${files[i].path}`
+      );
+      const fileName = files[i].name;
+      const path = files[i].path;
+      
+      createResetFile(path, ex.title, fileName);
+
+      editor = await vscode.window.showTextDocument(
+        file,
+        vscode.ViewColumn.One,
+        false
+      );
+    } catch (err) {
+      logger.error(err.message);
     }
-    return editor;
-}
+  }
+  return editor;
+};
