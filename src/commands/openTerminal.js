57ae23ca33cab4c43c25920966c6a8d4c70b369a
Amilcar Erazo <ajose.erazo@gmail.com>
2021-08-05 01:54:08 -0500

    Undo change

--- src/commands/openTerminal.js
+++ src/commands/openTerminal.js
@@ -6,31 +6,33 @@ const lp = require("../learnpack");
 let terminal = null;
 const TERMINAL_NAME = `LearnPack Terminal`;
 module.exports = () => {
-  const extConfig = vscode.workspace.getConfiguration();
-  const learnpackCommand = extConfig.get("learnpack.terminalEntryCommand");
+    const extConfig = vscode.workspace.getConfiguration()
+	const learnpackCommand = extConfig.get('learnpack.terminalEntryCommand');
+    // make sure there is  only one learnpack terminal
 
-  // make sure there is  only one learnpack terminal
-  if (
-    !terminal &&
-    vscode.window.activeTerminal &&
-    vscode.window.activeTerminal.name === TERMINAL_NAME
-  ) {
-    terminal = vscode.window.activeTerminal;
-  }
+    if(!terminal && vscode.window.activeTerminal && vscode.window.activeTerminal.name === TERMINAL_NAME){
+        terminal = vscode.window.activeTerminal
+    }
 
-  if (!terminal) {
-    logger.debug(`Opening new terminal`);
-    terminal = vscode.window.createTerminal(TERMINAL_NAME);
-    vscode.window.onDidCloseTerminal((t) => {
-      logger.debug(`Terminal closed`);
-      // ignore closing other terminals, only learnpack terminal matters
-      if (terminal.processId !== t.processId) return;
-
-      vscode.window.showInformationMessage(`LearnPack terminal closed`);
-      terminal = null;
-    });
-  } else
-    logger.debug(`Terminal already opened in process ${terminal.processId}`);
+    if(!terminal){
+        logger.debug(`Opening new terminal`)
+        terminal = vscode.window.createTerminal(TERMINAL_NAME);
+        vscode.window.onDidCloseTerminal(t => {
+            logger.debug(`Terminal closed`)
+            // ignore closing other terminals, only learnpack terminal matters
+            if(terminal.processId !== t.processId) return;
+            
+            vscode.window.showInformationMessage(`LearnPack terminal closed`);
+            terminal = null
+        });
+    } else logger.debug(`Terminal already opened in process ${terminal.processId}`)
+    
+    terminal.show(true); // true = don't take focus when opening
+    if(!lp.config().currentExercise){
+        logger.debug(`Running LearnPack from the terminal: ${learnpackCommand}`)
+        terminal.sendText(learnpackCommand);
+    }
+    else logger.debug(`LearnPack is already running, no need to start from the terminal`)
 
   terminal.show(true); // true = don't take focus when opening
   if (!lp.config().currentExercise) {
