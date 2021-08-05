091c4e497ac400c20b55707571380f2af7b4d627
Amilcar Erazo <ajose.erazo@gmail.com>
2021-08-05 01:47:15 -0500

    Fixing issue opening multiple editors

--- src/commands/openTerminal.js
+++ src/commands/openTerminal.js
@@ -1,41 +1,43 @@
+const fs = require("fs");
+const vscode = require("vscode");
+const logger = require("../utils/console");
+const lp = require("../learnpack");
 
-const fs = require('fs');
-const vscode = require('vscode');
-const logger = require('../utils/console')
-const lp = require('../learnpack')
-
-let terminal = null
-const TERMINAL_NAME = `LearnPack Terminal`
+let terminal = null;
+const TERMINAL_NAME = `LearnPack Terminal`;
 module.exports = () => {
+  const extConfig = vscode.workspace.getConfiguration();
+  const learnpackCommand = extConfig.get("learnpack.terminalEntryCommand");
 
-    const extConfig = vscode.workspace.getConfiguration()
-	const learnpackCommand = extConfig.get('learnpack.terminalEntryCommand');
-    
-    // make sure there is  only one learnpack terminal
-    if(!terminal && vscode.window.activeTerminal.name === TERMINAL_NAME){
-        terminal = vscode.window.activeTerminal
-    }
+  // make sure there is  only one learnpack terminal
+  if (
+    !terminal &&
+    vscode.window.activeTerminal &&
+    vscode.window.activeTerminal.name === TERMINAL_NAME
+  ) {
+    terminal = vscode.window.activeTerminal;
+  }
 
-    if(!terminal){
+  if (!terminal) {
+    logger.debug(`Opening new terminal`);
+    terminal = vscode.window.createTerminal(TERMINAL_NAME);
+    vscode.window.onDidCloseTerminal((t) => {
+      logger.debug(`Terminal closed`);
+      // ignore closing other terminals, only learnpack terminal matters
+      if (terminal.processId !== t.processId) return;
 
+      vscode.window.showInformationMessage(`LearnPack terminal closed`);
+      terminal = null;
+    });
+  } else
+    logger.debug(`Terminal already opened in process ${terminal.processId}`);
 
-        logger.debug(`Opening new terminal`)
-        terminal = vscode.window.createTerminal(TERMINAL_NAME);
-        vscode.window.onDidCloseTerminal(t => {
-            logger.debug(`Terminal closed`)
-            // ignore closing other terminals, only learnpack terminal matters
-            if(terminal.processId !== t.processId) return;
-            
-            vscode.window.showInformationMessage(`LearnPack terminal closed`);
-            terminal = null
-        });
-    } else logger.debug(`Terminal already opened in process ${terminal.processId}`)
-    
-    terminal.show(true); // true = don't take focus when opening
-    if(!lp.config().currentExercise){
-        logger.debug(`Running LearnPack from the terminal: ${learnpackCommand}`)
-        terminal.sendText(learnpackCommand);
-    }
-    else logger.debug(`LearnPack is already running, no need to start from the terminal`)
-
-}
+  terminal.show(true); // true = don't take focus when opening
+  if (!lp.config().currentExercise) {
+    logger.debug(`Running LearnPack from the terminal: ${learnpackCommand}`);
+    terminal.sendText(learnpackCommand);
+  } else
+    logger.debug(
+      `LearnPack is already running, no need to start from the terminal`
+    );
+};
